f0ff9bd9e520eb12737482338ccd38a6775eb43e
Equidem <matsochor@seznam.cz>
2021-06-10 13:19:01 +0200

    First functional version

--- src/fingerprint-generator.js
+++ src/fingerprint-generator.js
@@ -1,116 +1,119 @@
 const fs = require('fs');
 const path = require('path');
 const parse = require('csv-parse/lib/sync');
+const { default: ow } = require('ow');
+const HeaderGenerator = require('header-generator');
 
-const datasetPath = path.join(__dirname, "./dataset.csv");
-const arrayIndexRegexp = /^\d+\/{0,1}/;
+const { BayesianNetwork } = require("generative-bayesian-network");
+
+const fingerprintNetworkDefinition = require('./data_files/fingerprint-network-definition.json');
+
+const STRINGIFIED_PREFIX = '*STRINGIFIED*';
+const MISSING_VALUE_DATASET_TOKEN = '*MISSING_VALUE*';
 
 function getRandomInteger(minimum, maximum) {
     return minimum + Math.floor(Math.random() * (maximum - minimum + 1));
 }
 
-/*
- * This function is needed because saving the dataset in the CSV format leads to objects with attribute names like audioCodecs/mp3 or languages/0.
- * These have to be decoded back to the JSON structure before they can be used.
+const browserSpecificationShape = {
+    name: ow.string,
+    minVersion: ow.optional.number,
+    maxVersion: ow.optional.number,
+    httpVersion: ow.optional.string,
+};
+
+const headerGeneratorOptionsShape = {
+    browsers: ow.optional.array.ofType(ow.any(ow.object.exactShape(browserSpecificationShape), ow.string)),
+    operatingSystems: ow.optional.array.ofType(ow.string),
+    devices: ow.optional.array.ofType(ow.string),
+    locales: ow.optional.array.ofType(ow.string),
+    httpVersion: ow.optional.string,
+};
+
+/**
+ * @typedef BrowserSpecification
+ * @param {string} name - One of `chrome`, `firefox` and `safari`.
+ * @param {number} minVersion - Minimal version of browser used.
+ * @param {number} maxVersion - Maximal version of browser used.
+ * @param {string} httpVersion - Http version to be used to generate headers (the headers differ depending on the version).
+ *  Either 1 or 2. If none specified the httpVersion specified in `HeaderGeneratorOptions` is used.
  */
-function recursivelyRestoreJSONStructure(squashedObject) {
-    const recurseOn = new Set();
-    let restoredJSON = {};
-    let objectIsArray = Array.isArray(squashedObject);
-    if(objectIsArray) {
-        restoredJSON = new Array(squashedObject.length);
-    }
-
-    for(const attribute in squashedObject) {
-        let attributeToSplit = attribute;
-        let value = squashedObject[attribute];
-        if(objectIsArray) {
-            attributeToSplit = squashedObject[attribute][0];
-            value = squashedObject[attribute][1];
-        }
-
-        if(!attribute.includes("/")) {
-            restoredJSON[attribute] = value;
-        } else {            
-            let [ prefix, body, emptyString ] = attributeToSplit.split(/\/(.*)/);
-            
-            let attributeIsArray = false;
-            if(body.match(arrayIndexRegexp)) {
-                attributeIsArray = true;
-                body = body.replace(arrayIndexRegexp, "");
-            }
-
-            if(!recurseOn.has(prefix)) {
-                if(attributeIsArray) {
-                    restoredJSON[prefix] = [];
-                } else {
-                    restoredJSON[prefix] = {};
-                }
-            }
-
-            if(attributeIsArray) {
-                restoredJSON[prefix].push([body, value]);
-            } else {
-                restoredJSON[prefix][body] = value;
-            }
-            recurseOn.add(prefix);
-        }
-    }
-
-    for(const attribute of recurseOn) {
-        restoredJSON[attribute] = recursivelyRestoreJSONStructure(restoredJSON[attribute]);
-    }
-
-    return restoredJSON;
-}
+/**
+ * @typedef HeaderGeneratorOptions
+ * @param {Array<BrowserSpecification|string>} browsers - List of BrowserSpecifications to generate the headers for,
+ *  or one of `chrome`, `firefox` and `safari`.
+ * @param {Array<string>} operatingSystems - List of operating systems to generate the headers for.
+ *  The options are `windows`, `macos`, `linux`, `android` and `ios`.
+ * @param {Array<string>} devices - List of devices to generate the headers for. Options are `desktop` and `mobile`.
+ * @param {Array<string>} locales - List of at most 10 languages to include in the
+ *  [Accept-Language](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language) request header
+ *  in the language format accepted by that header, for example `en`, `en-US` or `de`.
+ * @param {string} httpVersion - Http version to be used to generate headers (the headers differ depending on the version).
+ *  Can be either 1 or 2. Default value is 2.
+ */
 
 /**
  * Fingerprint generator - randomly generates realistic browser fingerprints
  */
 class FingerprintGenerator {
 
-    constructor() {
-        const datasetText = fs.readFileSync(datasetPath, {encoding:'utf8'}).replace(/^\ufeff/, '');
-        const records = parse(datasetText, {
-            columns: true,
-            skip_empty_lines: true
-        });
-        
-        this.fingerprints = {};
-        for(const record of records) {
-            let userAgent = record["browserFingerprint/userAgent"];
-            let fingerprint = {};
-            for(const datasetAttribute of Object.keys(record)) {
-                const attribute = datasetAttribute.replace("www.", "").replace("browserFingerprint/", "");
-                if(record[datasetAttribute] !== "")
-                    fingerprint[attribute] = record[datasetAttribute];
-            }
-            if(!(userAgent in this.fingerprints)) {
-                this.fingerprints[userAgent] = [];
-            }
-
-            this.fingerprints[userAgent].push(fingerprint);
-        }
+    /**
+     * @param {HeaderGeneratorOptions} options - default header generation options used unless overridden
+     */
+    constructor(options = {}) {
+        ow(options, 'HeaderGeneratorOptions', ow.object.exactShape(headerGeneratorOptionsShape));
+        this.headerGenerator = new HeaderGenerator(options);
+        this.fingerprintGeneratorNetwork = new BayesianNetwork(fingerprintNetworkDefinition);
     }
 
     /**
-     * Generates a browser fingerprint consistent with the provided request headers
-     * @param {Object} requestHeaders - headers
+     * @param {HeaderGeneratorOptions} options - specifies options that should be overridden for this one call
+     * @param {Object} requestDependentHeaders - specifies known values of headers dependent on the particular request
      */
-    getFingerprint(requestHeaders) {
-        let userAgent = requestHeaders["user-agent"];
-        if(!userAgent) {
-            userAgent = requestHeaders["User-Agent"];
+    getFingerprint(options = {}, requestDependentHeaders = {}) {
+        ow(options, 'HeaderGeneratorOptions', ow.object.exactShape(headerGeneratorOptionsShape));
+        const headers = this.headerGenerator.getHeaders(options, requestDependentHeaders);
+        const userAgent = "User-Agent" in headers ? headers["User-Agent"] : headers["user-agent"];
+
+        console.log(headers);
+
+        let fingerprint = this.fingerprintGeneratorNetwork.generateSample({
+            "userAgent": userAgent
+        });
+
+        for(const attribute in fingerprint) {
+            if(fingerprint[attribute] == MISSING_VALUE_DATASET_TOKEN) {
+                delete fingerprint[attribute];
+            } else if(fingerprint[attribute].startsWith(STRINGIFIED_PREFIX)) {
+                fingerprint[attribute] = JSON.parse(fingerprint[attribute].slice(STRINGIFIED_PREFIX.length));
+            }
         }
 
-        if(!userAgent || !(userAgent in this.fingerprints)) {
-            throw new Error('No fingerprint can be generated for these headers.');
+        if("pluginCharacteristics" in fingerprint) {
+            for(const attribute in fingerprint["pluginCharacteristics"]) {
+                fingerprint[attribute] = fingerprint["pluginCharacteristics"][attribute];
+            }
+            delete fingerprint["pluginCharacteristics"];
         }
 
-        const fingerprintCandidates = this.fingerprints[userAgent];
+        if("screenCharacteristics" in fingerprint) {
+            for(const attribute in fingerprint["screenCharacteristics"]) {
+                fingerprint[attribute] = fingerprint["screenCharacteristics"][attribute];
+            }
+            delete fingerprint["screenCharacteristics"];
+        }
 
-        const fingerprint = fingerprintCandidates[getRandomInteger(0, fingerprintCandidates.length - 1)];
-        return recursivelyRestoreJSONStructure(fingerprint);
+        let acceptLanguageHeaderValue = "Accept-Language" in headers ? headers["Accept-Language"] : headers["accept-language"];
+        let acceptedLanguages = [];
+        for(const locale of acceptLanguageHeaderValue.split(",")) {
+            acceptedLanguages.push(locale.split(";")[0]);
+        }
+        fingerprint["languages"] = acceptedLanguages;
+
+        return {
+            fingerprint,
+            headers
+        };
     }
 }
 
--- test/example.test.js
+++ test/example.test.js
@@ -1,22 +1,16 @@
 const FingerprintGenerator = require('../src/main');
+const util = require('util')
 
 describe('Basic test', () => {
     const fingerprintGenerator = new FingerprintGenerator();
 
-    const headers = {
-      'upgrade-insecure-requests': '1',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
-      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-      'sec-fetch-site': 'same-site',
-      'sec-fetch-mode': 'navigate',
-      'sec-fetch-user': '?1',
-      'sec-fetch-dest': 'document',
-      'accept-encoding': 'gzip, deflate, br',
-      'accept-language': 'en-US'
-    };
-
     test('should pass', () => {
-        console.log(fingerprintGenerator.getFingerprint(headers));
+        for(let x = 0; x < 10000; x++) {
+            let fingerprint = fingerprintGenerator.getFingerprint({
+                "locales": ["en", "es", "en-US", "de"]
+            });
+            console.log(util.inspect(fingerprint, {showHidden: false, depth: null}))
+        }
         expect(true).toBeTruthy();
     });
 });
